aef9ed4e5de21ab458197c81dd01829073855cf5
zednaked <zednaked@gmail.com>
2025-07-09 15:01:41 -0300

    luz

--- build/index.service.worker.js
+++ build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1752080914|56746004';
+const CACHE_VERSION = '1752084095|1043194433';
 /** @type {string} */
 const CACHE_PREFIX = 'SirPeidalot-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
