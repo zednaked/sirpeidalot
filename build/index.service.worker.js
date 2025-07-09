de49c085895933516d6a785ef39151058b76f794
zednaked <zednaked@gmail.com>
2025-07-09 14:08:39 -0300

    biblio

--- build/index.service.worker.js
+++ build/index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1752080526|2200122334';
+const CACHE_VERSION = '1752080914|56746004';
 /** @type {string} */
-const CACHE_PREFIX = 'Trama-sw-cache-';
+const CACHE_PREFIX = 'SirPeidalot-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
