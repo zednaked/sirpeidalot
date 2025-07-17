a04620d11d979bcf7abf196501663ffff4910058
zednaked <zednaked@gmail.com>
2025-07-17 13:50:57 -0300

    inventario

--- build/index.service.worker.js
+++ build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1752680700|2092253998';
+const CACHE_VERSION = '1752771049|2815663744';
 /** @type {string} */
 const CACHE_PREFIX = 'SirPeidalot-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
