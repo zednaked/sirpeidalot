53ce0c0c165108d673233acd2bee3e847dac01be
zednaked <zednaked@gmail.com>
2025-07-25 17:54:00 -0300

    novos mapas

--- build/index.service.worker.js
+++ build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753476146|28445287';
+const CACHE_VERSION = '1753476829|145553526';
 /** @type {string} */
 const CACHE_PREFIX = 'SirPeidalot-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
